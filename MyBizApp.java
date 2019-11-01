50b599dfa04b4ec867ece60918b67c64469bc8dd
egoing <egoing@gmail.com>
2019-11-01 17:11:39 +0900

    프로그래밍의 기본 의미 수련

---
+++ MyBizApp.java
@@ -0,0 +1,10 @@
+
+public class MyBizApp {
+
+	public static void main(String[] args) {
+		System.out.println("Value Of Supply : "+10000);
+		System.out.println("VAT : "+10000*0.1);
+		System.out.println("Total : "+ (10000+(10000*0.1)) );
+	}
+
+}
