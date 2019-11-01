87c92475c6f69a67e6b13bac00c7067d5eb96a11
egoing <egoing@gmail.com>
2019-11-01 17:24:36 +0900

    클래스 도입

--- MyBizApp.java
+++ MyBizApp.java
@@ -1,16 +1,19 @@
 // java MyBizApp 10000
-public class MyBizApp {
-	public static double valueOfSupply = Double.parseDouble(args[0]);
+class Account{
+	public static double valueOfSupply = 0;
 	public static double vatRate = 0.1;
 	public static double getVAT() {
 		return valueOfSupply*vatRate;
 	}
 	public static double getTotal() {
-		return valueOfSupply*getVAT();
+		return valueOfSupply+getVAT();
 	}
+}
+public class MyBizApp {
 	public static void main(String[] args) {
-		System.out.println("Value Of Supply : "+valueOfSupply);
-		System.out.println("VAT : "+getVAT());
-		System.out.println("Total : "+ getTotal());
+		Account.valueOfSupply = Double.parseDouble(args[0]);
+		System.out.println("Value Of Supply : "+Account.valueOfSupply);
+		System.out.println("VAT : "+Account.getVAT());
+		System.out.println("Total : "+ Account.getTotal());
 	}
 }
