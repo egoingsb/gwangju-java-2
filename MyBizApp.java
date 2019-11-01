2281a3c0ae5c91e92216a9018f045ced5b8eb225
egoing <egoing@gmail.com>
2019-11-01 17:15:44 +0900

    변수 도입

--- MyBizApp.java
+++ MyBizApp.java
@@ -1,10 +1,11 @@
-
 public class MyBizApp {
-
 	public static void main(String[] args) {
-		System.out.println("Value Of Supply : "+10000);
-		System.out.println("VAT : "+10000*0.1);
-		System.out.println("Total : "+ (10000+(10000*0.1)) );
+		double valueOfSupply = 10000.0;
+		double vatRate = 0.1;
+		double vat = 10000*vatRate;
+		double total = valueOfSupply+vat;
+		System.out.println("Value Of Supply : "+valueOfSupply);
+		System.out.println("VAT : "+vat);
+		System.out.println("Total : "+ total );
 	}
-
 }
