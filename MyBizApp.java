ad127ef2270d78823eb52b9b78cd8848acf876d6
egoing <egoing@gmail.com>
2019-11-01 17:22:15 +0900

    메소드 도입

--- MyBizApp.java
+++ MyBizApp.java
@@ -1,12 +1,16 @@
 // java MyBizApp 10000
 public class MyBizApp {
+	public static double valueOfSupply = Double.parseDouble(args[0]);
+	public static double vatRate = 0.1;
+	public static double getVAT() {
+		return valueOfSupply*vatRate;
+	}
+	public static double getTotal() {
+		return valueOfSupply*getVAT();
+	}
 	public static void main(String[] args) {
-		double valueOfSupply = Double.parseDouble(args[0]);
-		double vatRate = 0.1;
-		double vat = 10000*vatRate;
-		double total = valueOfSupply+vat;
 		System.out.println("Value Of Supply : "+valueOfSupply);
-		System.out.println("VAT : "+vat);
-		System.out.println("Total : "+ total );
+		System.out.println("VAT : "+getVAT());
+		System.out.println("Total : "+ getTotal());
 	}
 }
